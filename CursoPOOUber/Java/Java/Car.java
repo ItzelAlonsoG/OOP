655364c74322ca7e226fd66560dc88b3b7524568
ItzelAlonsoG <itzel.alonso@accenture.com>
2021-07-01 17:18:23 -0500

    Declaracion de constructor para Java, JS y Python

--- CursoPOOUber/Java/Java/Car.java
+++ CursoPOOUber/Java/Java/Car.java
@@ -2,8 +2,17 @@
 
     class Car {
         Integer id;
-    String license;
-    String driver;
-    Integer passengers;
+        String license;
+        Account driver;
+        Integer passengers;
+
+        public Car(String license, Account driver){
+            this.license = license;
+            this.driver = driver;
+        }
+
+        void printDataCar() {
+            System.out.println("License: " + license + " Name Driver: " + driver.name);
+        }
 
     }
--- CursoPOOUber/Java/Java/main.java
+++ CursoPOOUber/Java/Java/main.java
@@ -1,8 +1,16 @@
 package Java;
 
-class Test {
+class Main {
     public static void main(String[] args) {
-     System.out.println("Hola Mundo!");   
+     System.out.println("Hola Mundo!"); 
+     Car car = new Car("AMQ1234", new Account("Andres Herrera", "AND123"));  
+     car.passengers = 2;
+     car.printDataCar();
+
+     Car car2 = new Car("QWE697", new Account("Andrea Herrera", "ANDA1232"));
+     car2.passengers = 3;
+     car2.printDataCar();
+
     }
 }
 
