711b6d0c2f8e239c0a1545c0912f89b0103fc38d
ItzelAlonsoG <itzel.alonso@accenture.com>
2021-07-01 19:29:04 -0500

    Aplicando Herencia en lenguaje de python y JS

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car ("AW123", new Account("Andres Herrera","QWEQWE"));
 car.passenger = 4;
 car.printDataCar();
+
+var Uberx = new UberX("AW1234", new Account("Itzel alonso","qeqw"),"Kia","2021");
+Uberx.printDataCar();
+console.log(Uberx);
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,5 +1,6 @@
 from car import Car
 from account import Account
+from UberX import UberX
 
 
 if __name__ == "__main__":
@@ -7,3 +8,7 @@ if __name__ == "__main__":
     car = Car("AMS234", Account("Andres Herrera","ANDA876"))
     print(vars(car))
     print(vars(car.driver))
+
+    uberX = UberX("ALTA123", Account("Andres Herrera","nada") ,"Tsuru", "2021")
+    print(vars(uberX))
+    print(vars(uberX.driver))
